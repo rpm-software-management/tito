a393dfbfda1c85c8a062895bcf57db196ab4dd90
Devan Goodwin <dgoodwin@rm-rf.ca>
2009-08-02 12:33:36 -0300

    Create a multi-project git repository for testing.

--- test/functional/functional-tests.py
+++ test/functional/functional-tests.py
@@ -27,11 +27,26 @@ from tito.common import check_tag_exists, commands, run_command
 # WARNING: This location will be destroyed if present.
 TEST_DIR = '/tmp/titotests/'
 SINGLE_GIT = os.path.join(TEST_DIR, 'single.git')
-#MULTI_GIT = os.path.join(TEST_DIR, 'multi.git')
+MULTI_GIT = os.path.join(TEST_DIR, 'multi.git')
 
-TEST_PKG_NAME = 'tito-test-pkg'
+TEST_PKG_1 = 'tito-test-pkg'
+TEST_PKG_1_DIR = "%s/" % TEST_PKG_1
+
+TEST_PKG_2 = 'tito-test-pkg2'
+TEST_PKG_2_DIR = "%s/" % TEST_PKG_2
+
+TEST_PKG_3 = 'tito-test-pkg3'
+TEST_PKG_3_DIR = "blah/whatever/%s/" % TEST_PKG_3
+
+TEST_PKGS = [
+        (TEST_PKG_1, TEST_PKG_1_DIR),
+        (TEST_PKG_2, TEST_PKG_2_DIR),
+        (TEST_PKG_3, TEST_PKG_3_DIR)
+]
+
+# NOTE: No Name in test spec file as we re-use it for several packages.
+# Name must be written first.
 TEST_SPEC = """
-Name:           tito-test-pkg
 Version:        0.0.1
 Release:        1%{?dist}
 Summary:        Tito test package.
@@ -81,54 +96,89 @@ def cleanup_temp_git():
         #error_out("Test Git repo already exists: %s" % TEST_DIR)
         run_command('rm -rf %s' % TEST_DIR)
 
-def create_temp_git(multi_project=False):
+def create_single_project_git():
     """ 
-    Create a test git repository. 
+    Create a test git repository with a single project, tito init, and create
+    an initial package tag.
     
     Repository will have a functional spec file for a package named 
     tito-test-pkg, be tito initiliazed and have one 0.0.1-1 tag created.
     This is *ALL* you should count on in these tests.
     """
-    cleanup_temp_git()
 
     run_command('mkdir -p %s' % TEST_DIR)
     run_command('mkdir -p %s' % SINGLE_GIT)
-#    run_command('mkdir -p %s' % MULTI_GIT)
     os.chdir(SINGLE_GIT)
+    run_command('git init')
+    run_tito("init")
+    run_command('echo "offline = true" >> rel-eng/tito.props')
+    run_command('git add rel-eng/tito.props')
+    run_command('git commit -m "Setting offline"')
+
+    create_git_project(SINGLE_GIT, TEST_PKG_1)
+
+def create_multi_project_git():
+    """ 
+    Create a test git repository with multiple projects, tito init, and
+    create initial tags for each package.
+    
+    Repository will have a functional spec file for a package named 
+    tito-test-pkg, be tito initiliazed and have one 0.0.1-1 tag created.
+    This is *ALL* you should count on in these tests.
+    """
+
+    run_command('mkdir -p %s' % TEST_DIR)
+    run_command('mkdir -p %s' % MULTI_GIT)
+    os.chdir(MULTI_GIT)
+    run_command('git init')
+    run_tito("init")
+    run_command('echo "offline = true" >> rel-eng/tito.props')
+    run_command('git add rel-eng/tito.props')
+    run_command('git commit -m "Setting offline"')
+
+    for pkg_name, pkg_dir in TEST_PKGS:
+        full_pkg_dir = os.path.join(MULTI_GIT, pkg_dir)
+        create_git_project(full_pkg_dir, pkg_name)
+
+def create_git_project(full_pkg_dir, pkg_name):
+    """
+    Write out test files for a git project at the specified location and 
+    commit them.
+    """
+    run_command('mkdir -p %s' % full_pkg_dir)
+    os.chdir(full_pkg_dir)
 
     # Write some files to the test git repo:
-    filename = os.path.join(SINGLE_GIT, "a.txt")
+    filename = os.path.join(full_pkg_dir, "a.txt")
     out_f = open(filename, 'w')
     out_f.write("BLERG")
     out_f.close()
 
     # Write the test spec file:
-    filename = os.path.join(SINGLE_GIT, "tito-test-pkg.spec")
+    filename = os.path.join(full_pkg_dir, "%s.spec" % pkg_name)
     out_f = open(filename, 'w')
+    out_f.write("Name: %s" % pkg_name)
     out_f.write(TEST_SPEC)
     out_f.close()
 
-    run_command('git init')
     run_command('git add a.txt')
-    run_command('git add tito-test-pkg.spec')
-    run_command('git commit -a -m "Initial commit."')
+    run_command('git add %s.spec' % pkg_name)
+    run_command('git commit -a -m "Initial commit for %s."' % pkg_name)
+
+    # Create initial 0.0.1 tag:
+    run_tito("tag --keep-version --accept-auto-changelog --debug")
+
 
 def setup_module():
     """ 
     Python Nose will run this once and only once for all tests in this 
     module. 
     """
-    create_temp_git()
+    cleanup_temp_git()
+    create_single_project_git()
+    create_multi_project_git()
 
     os.chdir(SINGLE_GIT)
-    run_tito("init")
-
-    run_command('echo "offline = true" >> rel-eng/tito.props')
-    run_command('git add rel-eng/tito.props')
-    run_command('git commit -m "Setting offline"')
-
-    # Create initial 0.0.1 tag:
-    run_tito("tag --keep-version --accept-auto-changelog --debug")
 
 def teardown_module():
     os.chdir('/tmp') # anywhere but the git repo were about to delete
@@ -148,24 +198,24 @@ class InitTests(unittest.TestCase):
             "tito.props")))
 
 
-class TaggerTests(unittest.TestCase):
+class SingleProjectTaggerTests(unittest.TestCase):
 
     def setUp(self):
         os.chdir(SINGLE_GIT)
 
     def test_initial_tag_keep_version(self):
         """ Create an initial package tag with --keep-version. """
-        check_tag_exists("%s-0.0.1-1" % TEST_PKG_NAME, offline=True)
+        check_tag_exists("%s-0.0.1-1" % TEST_PKG_1, offline=True)
         self.assertTrue(os.path.exists(os.path.join(SINGLE_GIT, 
-            "rel-eng/packages", TEST_PKG_NAME)))
+            "rel-eng/packages", TEST_PKG_1)))
 
     def test_initial_tag(self):
         """ Test creating an initial tag. """
         run_tito("tag --accept-auto-changelog --debug")
-        check_tag_exists("%s-0.0.2-1" % TEST_PKG_NAME, offline=True)
+        check_tag_exists("%s-0.0.2-1" % TEST_PKG_1, offline=True)
 
 
-class BuilderTests(unittest.TestCase):
+class SingleProjectBuilderTests(unittest.TestCase):
 
     def setUp(self):
         os.chdir(SINGLE_GIT)
@@ -174,16 +224,16 @@ class BuilderTests(unittest.TestCase):
         run_tito("build --tgz")
 
     def test_tag_tgz(self):
-        run_tito("build --tgz --tag=%s-0.0.1-1" % TEST_PKG_NAME)
+        run_tito("build --tgz --tag=%s-0.0.1-1" % TEST_PKG_1)
 
     def test_latest_srpm(self):
         run_tito("build --srpm")
 
     def test_tag_srpm(self):
-        run_tito("build --srpm --tag=%s-0.0.1-1" % TEST_PKG_NAME)
+        run_tito("build --srpm --tag=%s-0.0.1-1" % TEST_PKG_1)
 
     def test_latest_rpm(self):
         run_tito("build --rpm")
 
     def test_tag_rpm(self):
-        run_tito("build --rpm --tag=%s-0.0.1-1" % TEST_PKG_NAME)
+        run_tito("build --rpm --tag=%s-0.0.1-1" % TEST_PKG_1)
