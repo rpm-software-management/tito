53d0557ee5c525c8a5c3e1766102e1a48ced36c2
Miroslav Suchý <msuchy@redhat.com>
2010-04-23 13:59:28 +0200

    add section if it is in local config and not in global config
    addresing:
    Traceback (most recent call last):
      File "/home/msuchy/rhn/git-for-nightly-buils/rel-eng/bin/tito", line 26, in <module>
        CLI().main()
      File "/home/msuchy/rhn/git-for-nightly-buils/rel-eng/bin/../lib/spacewalk/releng/cli.py", line 123, in main
        module.main()
      File "/home/msuchy/rhn/git-for-nightly-buils/rel-eng/bin/../lib/spacewalk/releng/cli.py", line 355, in main
        build_dir)
      File "/home/msuchy/rhn/git-for-nightly-buils/rel-eng/bin/../lib/spacewalk/releng/cli.py", line 385, in _create_builder
        offline=options.offline)
      File "/home/msuchy/rhn/git-for-nightly-buils/rel-eng/bin/../lib/spacewalk/releng/builder.py", line 633, in __init__
        test=test, offline=offline)
      File "/home/msuchy/rhn/git-for-nightly-buils/rel-eng/bin/../lib/spacewalk/releng/builder.py", line 60, in __init__
        self.config.set(section, options, pkg_config.get(section, options))
      File "/usr/lib64/python2.6/ConfigParser.py", line 377, in set
        raise NoSectionError(section)
    ConfigParser.NoSectionError: No section: "buildconfig"

--- src/tito/builder.py
+++ src/tito/builder.py
@@ -66,6 +66,8 @@ class Builder(object):
         # Override global configurations using local configurations
         for section in pkg_config.sections():
             for options in pkg_config.options(section):
+                if not self.config.has_section(section):
+                    self.config.add_section(section)
                 self.config.set(section, options, 
                         pkg_config.get(section, options))
 
