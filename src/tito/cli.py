cd0da744852f25b1429c7f624bef378e7055b6ca
Devan Goodwin <dgoodwin@redhat.com>
2012-03-14 13:26:08 -0300

    Issue 39: Create /tmp/tito if it doesn't already exist.
    Because we treat this directory as a per-user directory (tito doesn't
    work well on a multi-user system, users have to configure their own
    output dirs with RPMBUILD_BASEDIR=~/tito/ in ~/.titorc), it seems best
    to do this internally to tito rather than try to handle the permissions
    in a spec file that won't know who should actually own it.

--- src/tito/cli.py
+++ src/tito/cli.py
@@ -130,6 +130,10 @@ class BaseCliModule(object):
             default=False)
 
         default_output_dir = lookup_build_dir(self.user_config)
+        if not os.path.exists(default_output_dir):
+            print("Creating output directory: %s" % default_output_dir)
+            run_command("mkdir %s" % default_output_dir)
+
         self.parser.add_option("-o", "--output", dest="output_dir",
                 metavar="OUTPUTDIR", default=default_output_dir,
                 help="Path to write temp files, tarballs and rpms to. "
