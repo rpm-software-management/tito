408b5cf051c032a5997eb6dd99fa37dba139ad93
Devan Goodwin <dgoodwin@redhat.com>
2013-07-08 14:30:54 -0300

    Automatic commit of package [tito] release [0.4.14-1].

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tito",
-    version='0.4.13',
+    version='0.4.14',
     description='A tool for managing rpm based git projects.',
     author='Devan Goodwin',
     author_email='dgoodwin@rm-rf.ca',
