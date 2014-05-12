576d46345faae9db3a28cfd6559891ff9783cd4f
Paul Morgan <jumanjiman@gmail.com>
2014-05-12 12:19:07 +0000

    make version comparison compat with python2 and python3
    Alas, python3 doesn't have the cmp() function.
    
    Support both python2 and python3 for comparisons using tip from
    https://docs.python.org/3.0/whatsnew/3.0.html#ordering-comparisons
    
    Avoids TB on python3:
    
        ======================================================================
        ERROR: test_compare_version (unit.common-tests.ExtractBugzillasTest)
        ----------------------------------------------------------------------
        Traceback (most recent call last):
          File "/home/sandbox/test/unit/common-tests.py", line 224, in test_compare_version
            self.assertEquals(0, compare_version("1", "1"))
          File "/home/sandbox/src/tito/common.py", line 689, in compare_version
            return cmp(normalize(version1), normalize(version2))
        NameError: global name 'cmp' is not defined

--- src/tito/common.py
+++ src/tito/common.py
@@ -686,4 +686,6 @@ def compare_version(version1, version2):
     """
     def normalize(v):
         return [int(x) for x in re.sub(r'(\.0+)*$', '', v).split(".")]
-    return cmp(normalize(version1), normalize(version2))
+    a = normalize(version1)
+    b = normalize(version2)
+    return (a > b) - (a < b)
