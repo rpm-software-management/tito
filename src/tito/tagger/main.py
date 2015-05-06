f86eba599dba9a2d870e618ff912ebdefe6b5319
Alex Wood <awood@redhat.com>
2015-05-06 17:36:39 -0400

    Use a context manager for editing the meta-data file.

--- src/tito/tagger/main.py
+++ src/tito/tagger/main.py
@@ -429,9 +429,9 @@ class VersionTagger(ConfigObject):
         # Write out our package metadata:
         metadata_file = os.path.join(self.rel_eng_dir, "packages",
                 self.project_name)
-        f = open(metadata_file, 'w')
-        f.write("%s %s\n" % (new_version_w_suffix, self.relative_project_dir))
-        f.close()
+
+        with open(metadata_file, 'w') as f:
+            f.write("%s %s\n" % (new_version_w_suffix, self.relative_project_dir))
 
         # Git add it (in case it's a new file):
         run_command("git add %s" % metadata_file)
