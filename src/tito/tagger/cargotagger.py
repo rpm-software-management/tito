4990b9e0491f90954c6116f0ec615af2aaaa6f8e
Sehny <sehnoutka.martin@gmail.com>
2016-11-21 18:22:48 +0100

    Implement cargo tagger using regular expressions (without toml library)

--- src/tito/tagger/cargotagger.py
+++ src/tito/tagger/cargotagger.py
@@ -1,5 +1,10 @@
+import re
+import os
 
-class CargoTagger():
+from tito.common import debug
+
+
+class CargoTagger:
     """
     Cargo is package manager for the Rust programming
     language: http://doc.crates.io/manifest.html
@@ -14,4 +19,42 @@ class CargoTagger():
         Find the line with version number  and change
         it to contain the new version.
         """
-        pass
+        if not os.path.exists(config_file):
+            debug('Config file was not found.')
+            return
+
+        debug("Found config file, attempting to update version.")
+        # We probably don't want version-release in config file as
+        # release is an RPM concept
+        rust_new_version = new_version.split('-')[0]
+        file_buffer = []
+
+        with open(config_file, 'r') as cfgfile:
+            pkg_label = re.compile('^\[package\]$')
+            label = re.compile('^\[.*\]$')
+            version = re.compile('(^version\s*=\s*)["\'](.+)["\'](.*$)')
+            lines = [line.rstrip('\n') for line in cfgfile]
+            state = 1
+            for line in lines:
+                # Looking for [package] label
+                if state == 1:
+                    file_buffer.append(line)
+                    if re.match(pkg_label, line):
+                        state = 2
+                elif state == 2:
+                    # Looking for version = "x.x.x" line
+                    if re.match(version, line):
+                        v = re.split(version, line)
+                        file_buffer.append(v[1] + '"' + rust_new_version + '"' + v[3])
+                        state = 3
+                    else:
+                        file_buffer.append(line)
+                    # if we found another label before version, it's probably not there
+                    if re.match(label, line):
+                        state = 3
+                # Just copy the rest of the file into the buffer
+                else:
+                    file_buffer.append(line)
+
+        with open(config_file, 'w') as cfgfile:
+            cfgfile.writelines(map(lambda x: x + "\n", file_buffer))
