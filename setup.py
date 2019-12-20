1d4bb0788a4ead5b4129ec05bb37486688cfd545
Jakub Kadlcik <frostyx@email.cz>
2019-12-20 17:19:06 +0100

    Automatic commit of package [tito] release [0.6.12-1].
    Created by command:
    
    /usr/bin/tito tag

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="tito",
-    version='0.6.11',
+    version='0.6.12',
     description='A tool for managing rpm based git projects.',
     author='Devan Goodwin',
     author_email='dgoodwin@rm-rf.ca',
